6a751534f5ffa567c510705e0c03ed8abf3b4b0f
silverbell <stackover92@gmail.com>
2023-09-01 10:43:10 +0900

    project init

---
+++ src/main/java/com/silverbell/lab/streamstudy/StreamStudyApplication.java
@@ -0,0 +1,13 @@
+package com.silverbell.lab.streamstudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StreamStudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StreamStudyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/silverbell/lab/streamstudy/StreamStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.silverbell.lab.streamstudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StreamStudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
